caae01b11f7cb3e1d4f01cce8828b1308c1b7ce8
Markus Reiter <me@reitermark.us>
2018-11-02 16:18:40 +0100

    Refactor deploy script.

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,35 +1,38 @@
 #!/bin/sh
 
+set -e
+set +o pipefail
+
 if ! [ -x "$(command -v docker)" ]; then
-  echo 'Unable to find docker command, please install Docker (https://www.docker.com/) and retry' >&2
+  echo 'Cannot find `docker` command, please install Docker (https://www.docker.com/) and retry.' >&2
   exit 1
 fi
 
 if ! [ -x "$(command -v faas-cli)" ]; then
-  case "$OSTYPE" in
-    darwin*|linux*)  curl -sSL https://cli.openfaas.com | sudo sh ;;
-    msys*|cygwin*)   choco install faas-cli -y ;;
-    *)               echo "unknown: $OSTYPE" ;;
+  case "$(uname -s)" in
+    Darwin*)
+      brew install faas-cli
+      ;;
+    linux*)
+      curl -sSL https://cli.openfaas.com | sudo -E sh
+      ;;
+    msys*|cygwin*)
+      choco install faas-cli -y
+      ;;
+    *)
+      echo 'Cannot find `faas-cli` command, please install the OpenFAAS CLI (https://docs.openfaas.com/cli/install/) and retry.' >&2
+      exit 1
+      ;;
   esac
-
-  echo 'Unable to find docker command, please install Docker (https://www.docker.com/) and retry' >&2
-  exit 1
 fi
 
-docker swarm init
-
-export BASIC_AUTH="true"
-
-sha_cmd="shasum -a 256"
-if ! command -v shasum >/dev/null; then
-  sha_cmd="sha256sum"
-fi
+export BASIC_AUTH='true'
 
 while [ ! $# -eq 0 ]
 do
 	case "$1" in
 		--no-auth | -n)
-			export BASIC_AUTH="false"
+			export BASIC_AUTH='false'
 			;;
     --help | -h)
 			echo "Usage: \n [default]\tdeploy the OpenFaaS core services\n --no-auth [-n]\tdisable basic authentication.\n --help\tdisplays this screen"
@@ -39,40 +42,53 @@ do
 	shift
 done
 
+# Initialize Swarm if not already created.
+docker swarm ca &>/dev/null || docker swarm init
+
+sha_cmd="shasum -a 256"
+if ! command -v shasum >/dev/null; then
+  sha_cmd="sha256sum"
+fi
+
 # Secrets should be created even if basic-auth is disabled.
-echo "Attempting to create credentials for gateway.."
-echo "admin" | docker secret create basic-auth-user -
-secret=$(head -c 16 /dev/urandom| $sha_cmd | cut -d " " -f 1)
-echo "$secret" | docker secret create basic-auth-password -
+echo 'Creating gateway credentials…'
 
-if [ $? = 0 ];
-then
-  echo "[Credentials]\n username: admin \n password: $secret\n echo -n "$secret" | faas-cli login --username=admin --password-stdin"
+user='admin'
+secret="$(head -c 16 /dev/urandom| $sha_cmd | cut -d ' ' -f 1)"
+
+echo '[Credentials]'
+echo "username: $user"
+echo "password: $secret"
+echo
+echo "printf '$secret' | faas-cli login --username=admin --password-stdin"
+echo
+
+docker secret rm basic-auth-user &>/dev/null|| true
+docker secret rm basic-auth-password &>/dev/null|| true
+
+printf "$user" | docker secret create basic-auth-user -
+printf "$secret" | docker secret create basic-auth-password -
+
+echo
+
+if [ "$BASIC_AUTH" = 'true' ]; then
+  echo 'Enabling basic authentication…'
 else
-  echo "[Credentials]\n already exist, not creating"
+  echo 'Disabling basic authentication…'
 fi
 
-if [ $BASIC_AUTH = "true" ];
-then
-  echo ""
-  echo "Enabling basic authentication for gateway.."
-  echo ""
-else
-  echo ""
-  echo "Disabling basic authentication for gateway.."
-  echo ""
-fi
+echo
 
-echo "Deploying OpenFaaS core services"
+echo 'Deploying OpenFaaS core services…'
 
 mkdir -p faas
-cp deploy.yml ./faas/deploy.yml
+cp -f deploy.yml ./faas/deploy.yml
 cd faas
 
 mkdir -p prometheus
 
-curl https://raw.githubusercontent.com/openfaas/faas/master/prometheus/alertmanager.yml -L -o ./prometheus/alertmanager.yml --silent
-curl https://raw.githubusercontent.com/openfaas/faas/master/prometheus/alert.rules.yml -L -o ./prometheus/alert.rules.yml --silent
-curl https://raw.githubusercontent.com/openfaas/faas/master/prometheus/prometheus.yml -L -o ./prometheus/prometheus.yml --silent
+curl -sL https://raw.githubusercontent.com/openfaas/faas/master/prometheus/alertmanager.yml -o ./prometheus/alertmanager.yml
+curl -sL https://raw.githubusercontent.com/openfaas/faas/master/prometheus/alert.rules.yml  -o ./prometheus/alert.rules.yml
+curl -sL https://raw.githubusercontent.com/openfaas/faas/master/prometheus/prometheus.yml   -o ./prometheus/prometheus.yml
 
 docker stack deploy func --compose-file deploy.yml
