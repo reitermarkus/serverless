94f95f7575fad9613020fa33ee4ea97365483e67
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-11-03 02:04:30 +0100

    Add `kafka-connector`, `kafka-topics-ui` and kafka producer script.

---
+++ produce.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SERVICE_NAME=func_kafka
+TASK_ID=$(docker service ps --filter 'desired-state=running' $SERVICE_NAME -q)
+CONTAINER_ID=$(docker inspect --format '{{ .Status.ContainerStatus.ContainerID }}' $TASK_ID)
+docker exec -it $CONTAINER_ID kafka-console-producer --broker-list kafka:9092 --topic faas-request
