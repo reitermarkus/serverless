56b2c50db514d1b95d6b3297d958b78fcdb57fef
Markus Reiter <me@reitermark.us>
2018-11-04 19:03:36 +0100

    Update scripts.

--- produce.sh
+++ produce.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+set +o pipefail
 
 SERVICE_NAME=func_kafka
-TASK_ID=$(docker service ps --filter 'desired-state=running' $SERVICE_NAME -q)
-CONTAINER_ID=$(docker inspect --format '{{ .Status.ContainerStatus.ContainerID }}' $TASK_ID)
-docker exec -it $CONTAINER_ID kafka-console-producer --broker-list kafka:9092 --topic faas-request
+TASK_ID=$(docker service ps --filter 'desired-state=running' "$SERVICE_NAME" -q)
+CONTAINER_ID=$(docker inspect --format '{{ .Status.ContainerStatus.ContainerID }}' "$TASK_ID")
+docker exec -it "$CONTAINER_ID" kafka-console-producer --broker-list kafka:9092 --topic faas-request
