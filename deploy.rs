30a8d822ff9c2950076dfd0eef684c1bf2e6b2a7
Markus Reiter <me@reitermark.us>
2018-11-15 06:26:13 +0100

    Update `deploy.rs`.

--- deploy.rs
+++ deploy.rs
@@ -38,9 +38,6 @@ use rand::{distributions::Alphanumeric, prelude::*};
 extern crate which;
 use which::which;
 
-extern crate dockworker;
-use dockworker::Docker;
-
 macro_rules! docker {
   () => {{
     Command::new("docker")
