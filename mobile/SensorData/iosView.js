d506baa2331cc700191c4a9e262113afc4fa3cc2
Markus Reiter <me@reitermark.us>
2019-02-16 21:28:34 +0100

    Fix warning on iOS.

--- mobile/SensorData/iosView.js
+++ mobile/SensorData/iosView.js
@@ -14,60 +14,30 @@ export default class IosView extends Component {
     super(props)
 
     this.state = {
-      accelerometer: {},
-      gyroscope: {},
-      magnetometer: {},
-      barometer: {}
+      accelerometer: {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+      },
+      barometer: {
+        pressure: 0.0,
+      },
+      gyroscope: {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+      },
+      magnetometer: {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+      },
     }
 
-    const round = (x, n) => Math.round(x * Math.pow(10, n)) / Math.pow(10, n)
-
     setUpdateIntervalForType(SensorTypes.accelerometer, 1000)
-    setUpdateIntervalForType(SensorTypes.gyroscope, 1000)
-    setUpdateIntervalForType(SensorTypes.barometer, 1000)
-    setUpdateIntervalForType(SensorTypes.magnetometer, 1000)
-
-    barometer.subscribe(({ pressure }) =>
-      this.setState({
-        barometer: {
-          pressure: round(pressure, 2) || 0,
-          timestamp: Date.now()
-        }
-      })
-    )
-
-    gyroscope.subscribe(({ x, y, z, timestamp }) =>
-      this.setState({
-        gyroscope: {
-          x: round(x, 5),
-          y: round(y, 5),
-          z: round(z, 5),
-          timestamp: timestamp
-        }
-      })
-    )
-
-    accelerometer.subscribe(({ x, y, z, timestamp }) =>
-      this.setState({
-        accelerometer: {
-          x: round(x, 5),
-          y: round(y, 5),
-          z: round(z, 5),
-          timestamp: timestamp
-        }
-      })
-    )
-
-    magnetometer.subscribe(({ x, y, z, timestamp }) =>
-      this.setState({
-        magnetometer: {
-          x: round(x, 5),
-          y: round(y, 5),
-          z: round(z, 5),
-          timestamp: timestamp
-        }
-      })
-    )
+    setUpdateIntervalForType(SensorTypes.gyroscope,     1000)
+    setUpdateIntervalForType(SensorTypes.barometer,     1000)
+    setUpdateIntervalForType(SensorTypes.magnetometer,  1000)
   }
 
   async componentDidMount() {
@@ -95,8 +65,57 @@ export default class IosView extends Component {
     }, 5000)
   }
 
+  componentWillMount() {
+    this.accelerometerSubscription = accelerometer.subscribe(({ x, y, z, timestamp }) =>
+      this.setState({
+        accelerometer: {
+          x: x,
+          y: y,
+          z: z,
+          timestamp: timestamp,
+        }
+      })
+    )
+
+    this.barometerSubscription = barometer.subscribe(({ pressure }) =>
+      this.setState({
+        barometer: {
+          pressure: pressure,
+          timestamp: Date.now(),
+        }
+      })
+    )
+
+    this.gyroscopeSubscription = gyroscope.subscribe(({ x, y, z, timestamp }) =>
+      this.setState({
+        gyroscope: {
+          x: x,
+          y: y,
+          z: z,
+          timestamp: timestamp,
+        }
+      })
+    )
+
+    this.magnetometerSubscription = magnetometer.subscribe(({ x, y, z, timestamp }) =>
+      this.setState({
+        magnetometer: {
+          x: x,
+          y: y,
+          z: z,
+          timestamp: timestamp,
+        }
+      })
+    )
+  }
+
   componentWillUnmount() {
     clearInterval(this.timer)
+
+    this.magnetometerSubscription.unsubscribe()
+    this.accelerometerSubscription.unsubscribe()
+    this.gyroscopeSubscription.unsubscribe()
+    this.barometerSubscription.unsubscribe()
   }
 
   render() {
@@ -138,7 +157,7 @@ export default class IosView extends Component {
               <Text>X</Text>
             </Left>
             <Right>
-              <Text>{this.state.gyroscope.x}</Text>
+              <Text>{this.state.gyroscope.x.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem>
@@ -146,7 +165,7 @@ export default class IosView extends Component {
               <Text>Y</Text>
             </Left>
             <Right>
-              <Text>{this.state.gyroscope.y}</Text>
+              <Text>{this.state.gyroscope.y.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem>
@@ -154,7 +173,7 @@ export default class IosView extends Component {
               <Text>Z</Text>
             </Left>
             <Right>
-              <Text>{this.state.gyroscope.z}</Text>
+              <Text>{this.state.gyroscope.z.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem itemDivider>
@@ -165,7 +184,7 @@ export default class IosView extends Component {
               <Text>X</Text>
             </Left>
             <Right>
-              <Text>{this.state.accelerometer.x}</Text>
+              <Text>{this.state.accelerometer.x.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem>
@@ -173,7 +192,7 @@ export default class IosView extends Component {
               <Text>Y</Text>
             </Left>
             <Right>
-              <Text>{this.state.accelerometer.y}</Text>
+              <Text>{this.state.accelerometer.y.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem>
@@ -181,7 +200,7 @@ export default class IosView extends Component {
               <Text>Z</Text>
             </Left>
             <Right>
-              <Text>{this.state.accelerometer.z}</Text>
+              <Text>{this.state.accelerometer.z.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem itemDivider>
@@ -192,7 +211,7 @@ export default class IosView extends Component {
               <Text>X</Text>
             </Left>
             <Right>
-              <Text>{this.state.magnetometer.x}</Text>
+              <Text>{this.state.magnetometer.x.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem>
@@ -200,7 +219,7 @@ export default class IosView extends Component {
               <Text>Y</Text>
             </Left>
             <Right>
-              <Text>{this.state.magnetometer.y}</Text>
+              <Text>{this.state.magnetometer.y.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem>
@@ -208,7 +227,7 @@ export default class IosView extends Component {
               <Text>Z</Text>
             </Left>
             <Right>
-              <Text>{this.state.magnetometer.z}</Text>
+              <Text>{this.state.magnetometer.z.toFixed(5)}</Text>
             </Right>
           </ListItem>
           <ListItem itemDivider>
@@ -219,7 +238,7 @@ export default class IosView extends Component {
               <Text>pressure</Text>
             </Left>
             <Right>
-              <Text>{this.state.barometer.pressure}</Text>
+              <Text>{this.state.barometer.pressure.toFixed(5)}</Text>
             </Right>
           </ListItem>
         </List>
