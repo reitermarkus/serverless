100407cd877c9f68d97547f0253b73f3b4bffe51
Markus Reiter <me@reitermark.us>
2019-07-11 07:30:00 +0200

    More awaiting.

--- functions/template/rust-http/main/src/main.rs
+++ functions/template/rust-http/main/src/main.rs
@@ -3,8 +3,8 @@
 use std::fmt::{self, Display, Debug};
 use std::error::Error;
 
-use futures01::{future::{self, Future}, stream::Stream};
-use futures::future::{FutureExt, TryFutureExt};
+use futures01::stream::Stream;
+use futures::{compat::Future01CompatExt, future::{FutureExt, TryFutureExt}};
 use gotham::{self, handler::{HandlerFuture, IntoHandlerError}, helpers::http::response::create_response, router::{builder::*, Router}, state::{FromState, State}};
 use hyper::Body;
 use http::{Method, HeaderMap, Uri};
@@ -39,47 +39,33 @@ impl Error for HandlerErrorWrapper {
 }
 
 pub fn handler(mut state: State) -> Box<HandlerFuture> {
-  let f = Body::take_from(&mut state)
-    .concat2()
-    .then(|res| match res {
-      Ok(body) => {
-        match String::from_utf8(body.to_vec()) {
-          Ok(content) => future::ok(content),
-          Err(err) => future::err(err.into_handler_error()),
-        }
+  Box::new(async {
+    let body = match Body::take_from(&mut state).concat2().compat().await {
+      Ok(body) => match String::from_utf8(body.to_vec()) {
+        Ok(content) => content,
+        Err(err) => return Err((state, err.into_handler_error())),
       },
-      Err(err) => future::err(err.into_handler_error()),
-    })
-    .then(|res| {
-      let f = async {
-        match res {
-          Ok(body) => {
-            let method = http::Method::borrow_from(&state).to_owned();
-            let headers = HeaderMap::borrow_from(&state).to_owned();
-            let uri = Uri::borrow_from(&state).to_owned();
+      Err(err) => return Err((state, err.into_handler_error())),
+    };
 
-            match handle(method, uri, headers, body).await {
-              Ok((status_code, response)) => {
-                let res = create_response(
-                    &state,
-                    status_code,
-                    mime::TEXT_PLAIN,
-                    response,
-                );
+    let method = http::Method::borrow_from(&state).to_owned();
+    let headers = HeaderMap::borrow_from(&state).to_owned();
+    let uri = Uri::borrow_from(&state).to_owned();
 
-                Ok((state, res))
-              },
-              Err(err) => Err((state, HandlerErrorWrapper { cause: err }.into_handler_error())),
-            }
-          },
-          Err(err) => Err((state, err.into_handler_error())),
-        }
-      };
+    match handle(method, uri, headers, body).await {
+      Ok((status_code, response)) => {
+        let res = create_response(
+            &state,
+            status_code,
+            mime::TEXT_PLAIN,
+            response,
+        );
 
-      f.boxed().compat()
-    });
-
-  Box::new(f)
+        Ok((state, res))
+      },
+      Err(err) => Err((state, HandlerErrorWrapper { cause: err }.into_handler_error())),
+    }
+  }.boxed().compat())
 }
 
 fn router() -> Router {
