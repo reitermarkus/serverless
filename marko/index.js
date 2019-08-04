a8fc0c654d3a0c6e1428926f7aaa503114b681b2
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2019-08-05 01:58:15 +0200

    Add initial Marko app.

---
+++ marko/index.js
@@ -0,0 +1,3 @@
+const conponent = require("./index.marko");
+
+conponent.renderSync().appendTo(document.body);
---
+++ marko/webpack.config.js
@@ -0,0 +1,29 @@
+//based on https://github.com/marko-js-samples/marko-webpack/blob/master/webpack.config.js
+const { NODE_ENV } = process.env;
+const isProd = NODE_ENV === "production";
+const isDev = !isProd;
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: __dirname,
+    filename: "static/bundle.js"
+  },
+  devServer: isDev ? {
+    overlay: true,
+    stats: "minimal",
+    contentBase: __dirname,
+    historyApiFallback: true
+  } : undefined,
+  resolve: {
+    extensions: [".js", ".marko"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.marko$/,
+        loader: "marko-loader"
+      }
+    ]
+  }
+};
