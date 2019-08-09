fb077bfeddaa8392625472100ad657b0c11e6417
Markus Reiter <me@reitermark.us>
2019-08-09 03:45:40 +0200

    Refactor `log-rpi` function.

--- functions/log-rpi/src/lib.rs
+++ functions/log-rpi/src/lib.rs
@@ -1,74 +1,47 @@
 #![feature(async_await)]
 
+use std::collections::HashMap;
 use std::error::Error;
 
 use http::{HeaderMap, Method, Uri, StatusCode};
 use serde_derive::{Deserialize, Serialize};
-use serde_json::{self, json};
+use serde_json::{self, json, Value};
 
 use openfaas;
 
-#[derive(Debug, Deserialize, Serialize)]
-struct IlluminanceData {
-  pub illuminance: f64,
-  pub time: String,
-}
+const SUPPORTED_DATA_TYPES: [&'static str; 5] = [
+  "pressure",
+  "cpu_temperature",
+  "illuminance",
+  "temperature",
+  "humidity",
+];
 
-#[derive(Debug, Deserialize, Serialize)]
-struct PressureData {
-  pub pressure: f64,
-  pub time: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct TemperatureData {
-  pub temperature: f64,
-  pub time: String,
-}
-
-pub async fn handle(method: Method, _uri: Uri, _headers: HeaderMap, body: String) -> Result<(StatusCode, String), Box<dyn Error + Send>> {
+pub async fn handle(method: Method, uri: Uri, headers: HeaderMap, body: String) -> Result<(StatusCode, String), Box<dyn Error + Send>> {
   if method != Method::POST {
-    return Ok((StatusCode::METHOD_NOT_ALLOWED, "".to_string()))
+    return Ok((StatusCode::METHOD_NOT_ALLOWED, format!("Method '{}' is not allowed.", method)))
   }
 
-  let mut status_code = StatusCode::INTERNAL_SERVER_ERROR;
-  let mut response = "".to_string();
+  let mut map = match serde_json::from_str::<HashMap<String, Value>>(&body) {
+    Ok(map) => map,
+    _ => return Ok((StatusCode::BAD_REQUEST, "Invalid format.".to_string())),
+  };
 
-  if let Ok(illuminance_data) = serde_json::from_str::<IlluminanceData>(&body) {
-    if let Ok((s, r)) = openfaas::call("database", json!({
-      "collection": "illuminances",
-      "action": "insert",
-      "doc": illuminance_data,
-    }).to_string()).await {
-      status_code = s;
-      response.push_str(&r);
-      response.push('\n');
-    }
+  let data_type = match map.remove("type") {
+    Some(value) => match serde_json::from_value::<String>(value) {
+      Ok(s) => s,
+      _ => return Ok((StatusCode::BAD_REQUEST, "Invalid format.".to_string())),
+    },
+    _ => return Ok((StatusCode::BAD_REQUEST, "No 'type' found.".to_string())),
+  };
+
+  if !SUPPORTED_DATA_TYPES.iter().any(|&t| t == data_type) {
+    return Ok((StatusCode::BAD_REQUEST, format!("Data type '{}' is not supported.", data_type)));
   }
 
-  if let Ok(pressure_data) = serde_json::from_str::<PressureData>(&body) {
-    if let Ok((s, r)) = openfaas::call("database", json!({
-      "collection": "pressures",
-      "action": "insert",
-      "doc": pressure_data,
-    }).to_string()).await {
-      status_code = s;
-      response.push_str(&r);
-      response.push('\n');
-    }
-  }
-
-  if let Ok(temperature_data) = serde_json::from_str::<TemperatureData>(&body) {
-    if let Ok((s, r)) = openfaas::call("database", json!({
-      "collection": "temperatures",
-      "action": "insert",
-      "doc": temperature_data,
-    }).to_string()).await {
-      status_code = s;
-      response.push_str(&r);
-      response.push('\n');
-    }
-  }
-
-  Ok((status_code, response))
+  openfaas::call("database", json!({
+    "collection": data_type,
+    "action": "insert",
+    "doc": map,
+  }).to_string()).await
 }
