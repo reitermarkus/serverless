987156dbb56bc09b12dd959cd07e0928c6edc417
Markus Reiter <me@reitermark.us>
2019-11-17 02:12:15 +0100

    Remove React Native App.

--- mobile/SensorData/ios/Toast.m
+++
@@ -1,7 +0,0 @@
-#import "React/RCTBridgeModule.h"
-
-@interface RCT_EXTERN_REMAP_MODULE(ToastIOS, Toast, NSObject)
-
-RCT_EXTERN_METHOD(show: (nonnull NSString)message duration:(nonnull NSNumber)duration)
-
-@end
